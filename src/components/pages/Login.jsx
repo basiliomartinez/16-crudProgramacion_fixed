6d10b719646ce12fd2d352668254032d11752b0a
basiliomartinez <basilioezequielmartinez@gmail.com>
2025-11-16 13:02:54 -0300

    Agregamos el maquetado de Inicio y Card Servicio

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,9 +1,33 @@
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 const Login = () => {
     return (
-        <div>
-            <p>Pagina Login</p>
-        </div>
+  <main className='container
+  '>
+    <h1>Login</h1>
+    <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-danger">
+       Error validacion
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Contraseña</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+           <Form.Text className="text-danger">
+       Error validacion
+        </Form.Text>
+      </Form.Group>
+      
+      <Button variant="primary" type="submit">
+        Enviar
+      </Button>
+    </Form>
+  </main>
     );
 };
 
--- src/components/services/CardServicio.jsx
+++ src/components/services/CardServicio.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import { Card, Button, Col } from 'react-bootstrap';
+
 
 const CardServicio = () => {
     return (
-        <div>
-            <p>Card Servicio</p>
-        </div>
+    <Col>
+       <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="https://images.pexels.com/photos/230544/pexels-photo-230544.jpeg" />
+      <Card.Body>
+        <Card.Title>Sitio Web Institucional (5-7 Paginas)</Card.Title>
+        <Card.Text>
+                   Web profesional *responsive* con secciones clave: Inicio, Servicios, Quiénes Somos, Contacto y Blog (sin funcionalidad de publicación).
+        </Card.Text>
+        <Card.Text>  
+            Precio: $ 300.000
+        </Card.Text>
+        <Button variant="primary">Ver detalle</Button>
+      </Card.Body>
+    </Card>
+    </Col>
     );
 };
 

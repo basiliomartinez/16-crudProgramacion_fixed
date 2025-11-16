46ff7cb0ab2b27f4d02806025c91d94fb08b222d
basiliomartinez <basilioezequielmartinez@gmail.com>
2025-11-16 13:45:26 -0300

    Agregamos el maquetado de la pagina del administrador

--- src/components/pages/Administrador.jsx
+++ src/components/pages/Administrador.jsx
@@ -1,9 +1,28 @@
+import Table from 'react-bootstrap/Table';
+import ItemTabla from '../services/ItemTabla';
+import { Button } from 'react-bootstrap';
 
 const Administrador = () => {
     return (
-        <div>
-           <p>Administrador</p>
-        </div>
+    <main className='container my-4'>
+        <div className='d-flex justify-content-between align-items-center'>
+        <h1>Administrar Servicios</h1>
+<Button>Crear</Button>
+</div>
+         <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Servicio</th>
+          <th>Precio</th>
+          <th>Acciones</th>
+        </tr>
+      </thead>
+      <tbody>
+    <ItemTabla></ItemTabla>
+    </tbody>
+    </Table>
+    </main>
     );
 };
 

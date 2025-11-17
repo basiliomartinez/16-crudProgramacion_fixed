1ad78113da79ea50f6cd7fc2f984fab6f860518d
basiliomartinez <basilioezequielmartinez@gmail.com>
2025-11-17 09:24:45 -0300

    Logica del Local Storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,51 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import Inicio from "./components/pages/Inicio";
 import Login from "./components/pages/Login";
-import DetalleServicios from "./components/pages/DetalleServicio"
+import DetalleServicios from "./components/pages/DetalleServicio";
 import Administrador from "./components/pages/Administrador";
 import FormularioServicio from "./components/pages/FormularioServicio";
 import Error404 from "./components/pages/Error404";
 import Menu from "./components/shared/Menu";
 import Footer from "./components/shared/Footer";
-
+import { useEffect, useState } from "react";
 
 function App() {
+  const usuarioSessionStorage = JSON.parse(
+    sessionStorage.getItem("usuarioKey") || false
+  );
+
+  const [usuarioLogeado, setUsuarioLogeado] = useState (usuarioSessionStorage);
+
+  useEffect(()=>{
+    sessionStorage.setItem('usuarioKey', JSON.stringify(usuarioLogeado))
+  }, [usuarioLogeado])
+
   return (
     <BrowserRouter>
-    <Menu></Menu>
+      <Menu usuarioLogeado={usuarioLogeado} setUsuarioLogeado={setUsuarioLogeado}
+      ></Menu>
       <Routes>
         <Route path="/" element={<Inicio></Inicio>} />
-        <Route path="/login" element={<Login></Login>} />
-        <Route path="/detalle" element={<DetalleServicios></DetalleServicios>} />
-        <Route path="/administrador" element={<Administrador></Administrador>} />
-        <Route path="/administrador/crear" element={<FormularioServicio></FormularioServicio>} />
-        <Route path="/administrador/editar" element={<FormularioServicio></FormularioServicio>} />
+        <Route path="/login" element={<Login setUsuarioLogeado={setUsuarioLogeado}></Login>} />
+        <Route
+          path="/detalle"
+          element={<DetalleServicios></DetalleServicios>}
+        />
+        <Route
+          path="/administrador"
+          element={<Administrador></Administrador>}
+        />
+        <Route
+          path="/administrador/crear"
+          element={<FormularioServicio></FormularioServicio>}
+        />
+        <Route
+          path="/administrador/editar"
+          element={<FormularioServicio></FormularioServicio>}
+        />
         <Route path="*" element={<Error404></Error404>} />
-        </Routes>
-        <Footer></Footer>
+      </Routes>
+      <Footer></Footer>
     </BrowserRouter>
   );
 }
--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,34 +1,83 @@
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router";
+import Swal from "sweetalert2";
 const Login = () => {
-    return (
-  <main className='container
-  '>
-    <h1>Login</h1>
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-danger">
-       Error validacion
-        </Form.Text>
-      </Form.Group>
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Contraseña</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-           <Form.Text className="text-danger">
-       Error validacion
-        </Form.Text>
-      </Form.Group>
-      
-      <Button variant="primary" type="submit">
-        Enviar
-      </Button>
-    </Form>
-  </main>
-    );
+  const onSubmit = (data) => {
+    console.log(data);
+    if (data.email===import.meta.env.VITE_EMAIL && data.password===import.meta.env.VITE_PASSWORD){
+      //redireccionaria a la pagina del administrador
+      //mostrar un cartel de bienvenido
+       Swal.fire({
+  title: "Bienvenido Administrador",
+  text: "Ingresando al sistema",
+  icon: "success"
+});  
+    }else{
+      Swal.fire({
+  title: "Ocurrio un error",
+  text: "Credenciales Incorrectas!",
+  icon: "error"
+});  
+    }
+  };
+
+  return (
+    <main
+      classNa me="container
+  "
+    >
+      <h1>Login</h1>
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            {...register("email", {
+              require: "el email es un dato obligatorio",
+              pattern: {
+                value:
+                  /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/,
+                message:
+                  "El email debe cumplir con el siguiente formato correo@dominio.extension",
+              },
+            })}
+          />
+          <Form.Text className="text-danger">{errors.email?.message}</Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Contraseña</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            {...register("password", {
+              require: "La contraseña es un dato obligatorio",
+              pattern: {
+                value:
+                  /^(?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])(?=.*[A-Z])(?=.*[a-z])\S{8,16}$/,
+                message:
+                  "La contraseña debe tener entre 8 y 16 caracteres, al menos un dígito, al menos una minúscula, al menos una mayúscula y al menos un caracter especial.",
+              },
+            })}
+          />
+          <Form.Text className="text-danger">{errors.password?.message}</Form.Text>
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Enviar
+        </Button>
+      </Form>
+    </main>
+  );
 };
 
 export default Login;
